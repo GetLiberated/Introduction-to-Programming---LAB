f6aa7843dd1b0411da87afa74abadec779f6d0d4
savageRex <43334192+savageRex@users.noreply.github.com>
2018-09-24 15:00:27 +0700

    Update Week 01 Assignment - ITP_TA - Looping.py

--- Week 01 Assignment - ITP_TA - Looping.py
+++ Week 01 Assignment - ITP_TA - Looping.py
@@ -1,24 +1,29 @@
-lebar=int(input("Enter number of rows: "))
-for y in range(0,lebar):
-    for x in range(0,y+1):
-        print('*', end=" ")
-    print()
-print()
+lebar=int(input("Enter number of rows: ")) # This is where you input the number
 
-for y in range(0, lebar):
-    for x in range(0,lebar-y):
+#Triangle 1
+for y in range(0,lebar): # The range of Y axis from 0 to total number of the rows you entered.
+    for x in range(0,y+1): # The range of X axis from 0 to total number of y plus 1.
+        print('*', end=" ") # Print asteriskt then space within the X range.
+    print() # Print a new line after each asterisk and space printed.
+print() # Print a new line to seperate this triangle with below triangle.
+
+#Triangle 2
+for y in range(0, lebar): # The range of Y axis from 0 to total number of the rows you entered.
+    for x in range(0,lebar-y): # The range of X axis from 0 to total number of the rows you entered minus the Y range.
         print(end=" ")
     for x in range(0,y+1):
         print('*',end='')
     print()
 print()
 
+#Triangle 3
 for y in range(0,lebar+1):
     for x in range(0,lebar-y):
         print('*', end=" ")
     print()
 print()
 
+#Triangle 4
 for y in range(0, lebar):
     for x in range(0,lebar+y):
         print(end=" ")
@@ -27,6 +32,7 @@ for y in range(0, lebar):
     print()
 print()
 
+#Triangle 5
 for y in range(0,lebar):
     for x in range(0,lebar-y):
         print(end=" ")
@@ -35,6 +41,7 @@ for y in range(0,lebar):
     print()
 print()
 
+#Triangle 6
 for y in range(0, lebar):
     for x in range(0,lebar-y):
         print(end=" ")
