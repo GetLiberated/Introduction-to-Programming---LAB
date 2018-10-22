cf7f6566335318d77458d037c0d2ce69750f3eaf
Eris Suryaputra <eriso.o77@gmail.com>
2018-10-22 14:55:48 +0700

    Update Assignment 06 - Projectile.py

--- Assignment 06 - Projectile.py
+++ Assignment 06 - Projectile.py
@@ -3,9 +3,6 @@ import matplotlib.pylab as plot
 import math as m
 import sys
 
-x = [0]
-y = [0]
-
 num = int(input('How many projectile you want to create? >> '))+1 # Create how many graph
 
 for i in range(1,num):
@@ -17,12 +14,10 @@ for i in range(1,num):
     t = ((2*v)*np.sin(angle))/g   # Formula to find time
     ts = t*np.linspace(0,1)  # Time vector
 
-    xi = (v*ts)*np.cos(angle)  # Formula to find the x position
-    yi = (v*ts)*np.sin(angle) - (g/2)*(ts**2)  # Formula to find y position
-    x.append(xi)
-    y.append(yi)
+    x = (v*ts)*np.cos(angle)  # Formula to find the x position
+    y = (v*ts)*np.sin(angle) - (g/2)*(ts**2)  # Formula to find y position
 
-    plot.plot(x[i],y[i],label='Projectile no {}'.format(i)) # Create line graph with label
+    plot.plot(x,y,label='Projectile no {}'.format(i)) # Create line graph with label
 
 plot.title('Projection motion of a ball') # Graph title
 plot.xlabel('x-coordinate') # Graph x label title
